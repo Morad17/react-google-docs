7d373f1e3c830f2ffc01f65eb98d091e1a362b5a
Morad17 <morad123@hotmail.co.uk>
2021-04-27 02:19:48 +0100

    initial commit

---
+++ src/textEditor.js
@@ -0,0 +1,25 @@
+import React, { useCallback, useRef } from 'react'
+import Quill from "quill"
+import "quill/dist/quill.snow.css"
+
+
+const TextEditor = (props) => {
+
+    const wrapperRef = useCallback((wrapper) => {
+        if (wrapper == null) return
+
+        wrapper.innerHTML = ''
+        const editor = document.createElement('div')
+        wrapper.append(editor)
+        new Quill(editor, {theme: "snow"})
+    }, [])
+
+
+
+  return(
+    <div className="container" ref={wrapperRef}>TextEditor</div>
+   )
+  }
+
+
+export default TextEditor
